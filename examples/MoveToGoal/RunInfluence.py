8ddcc4f8eb1e48cb04a9358b8e3f8004c7d4782f
yalyzak <yaly.zak@gmail.com>
2026-08-27 13:20:07 +0300

    bigger scene

--- examples/MoveToGoal/RunInfluence.py
+++ examples/MoveToGoal/RunInfluence.py
@@ -6,20 +6,20 @@ from MoveToGoal import MoveToGoal
 
 cam = GameObject(position=Vector3(0, 5, 0), rotation=Vector3(90,0,0)).add_component(Camera(), CamController(), FPS_cam())
 
-floor = GameObject(position=Vector3(0,-1,0), size=Vector3(10,1,10)).add_component(BoxCollider(), Rigidbody(isKinematic=True, friction_coefficient=1))
-wall1 = GameObject(position=Vector3(-5.5,0,0), size=Vector3(1,1,10)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
-wall2 = GameObject(position=Vector3(5.5,0,0), size=Vector3(1,1,10)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
-wall3 = GameObject(position=Vector3(0,0,-5.5), size=Vector3(10,1,1)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
-wall4 = GameObject(position=Vector3(0,0,5.5), size=Vector3(10,1,1)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
+floor = GameObject(position=Vector3(0,-1,0), size=Vector3(50,1,50)).add_component(BoxCollider(), Rigidbody(isKinematic=True, friction_coefficient=1))
+wall1 = GameObject(position=Vector3(-25,0,0), size=Vector3(1,1,50)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
+wall2 = GameObject(position=Vector3(25,0,0), size=Vector3(1,1,50)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
+wall3 = GameObject(position=Vector3(0,0,-25), size=Vector3(50,1,1)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
+wall4 = GameObject(position=Vector3(0,0,25), size=Vector3(50,1,1)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
 
 goal = GameObject(position=Vector3(2,0,0)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Goal())
 
 config = Config(
-    obs_dim=6,
+    obs_dim=9,
     action_dim_continuous=2,
     rollout_steps=1024,
     device="cpu",
-    # max_steps=30
+    max_steps=50
 )
 Academy.setup_trainer(config)
 Academy.load_model("model.pt")
--- examples/MoveToGoal/TrainSingelAgent.py
+++ examples/MoveToGoal/TrainSingelAgent.py
@@ -6,21 +6,21 @@ from MoveToGoal import MoveToGoal
 
 cam = GameObject(position=Vector3(0, 5, 0), rotation=Vector3(90,0,0)).add_component(Camera(), CamController(), FPS_cam())
 
-floor = GameObject(position=Vector3(0,-1,0), size=Vector3(10,1,10)).add_component(BoxCollider(), Rigidbody(isKinematic=True, friction_coefficient=1))
-wall1 = GameObject(position=Vector3(-5.5,0,0), size=Vector3(1,1,10)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
-wall2 = GameObject(position=Vector3(5.5,0,0), size=Vector3(1,1,10)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
-wall3 = GameObject(position=Vector3(0,0,-5.5), size=Vector3(10,1,1)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
-wall4 = GameObject(position=Vector3(0,0,5.5), size=Vector3(10,1,1)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
+floor = GameObject(position=Vector3(0,-1,0), size=Vector3(50,1,50)).add_component(BoxCollider(), Rigidbody(isKinematic=True, friction_coefficient=1))
+wall1 = GameObject(position=Vector3(-25,0,0), size=Vector3(1,1,50)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
+wall2 = GameObject(position=Vector3(25,0,0), size=Vector3(1,1,50)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
+wall3 = GameObject(position=Vector3(0,0,-25), size=Vector3(50,1,1)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
+wall4 = GameObject(position=Vector3(0,0,25), size=Vector3(50,1,1)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Wall())
 
 goal = GameObject(position=Vector3(2,0,0)).add_component(BoxCollider(), Rigidbody(isKinematic=True), Goal())
 
 config = Config(
-    obs_dim=6,
+    obs_dim=9,
     action_dim_continuous=2,
-    rollout_steps=30,
+    rollout_steps=5000,
     device="cpu",
     best_model_path="model.pt",
-    max_steps=200
+    max_steps=50
 
 )
 
@@ -31,6 +31,6 @@ agent = GameObject().add_component(BoxCollider(), Rigidbody(Freeze_Rotation=Vect
 
 main = GameObject(children=[floor, wall1, wall2, wall3, wall4, goal, agent], size=Vector3(0,0,0))
 
-scene = [main]
+scene = [main, cam]
 
 Core.run_max_speed(scene, Render=False, MaxTime=60*60)  # train for 1 simulated hour
